d7b8055adb0a06912014e2c32cbaa39db6f71cc0
unknown <alvinafandi80@gmail.com>
2018-06-26 01:50:59 +0700

    layout dan icon clear

---
+++ app/src/main/java/com/example/alvinafandi/on_ukm/Oprec.java
@@ -0,0 +1,13 @@
+package com.example.alvinafandi.on_ukm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Oprec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oprec);
+    }
+}
